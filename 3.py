167ba14c67060230b69b02bac3b5abb573961f08
karelbondan <71486691+karelbondan@users.noreply.github.com>
2020-09-21 13:16:38 +0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -5,9 +5,7 @@ Created on Mon Sep 21 11:19:56 2020
 @author: karel
 """
 
-cls1 = eval(input("Input the number of students in class 1= ", ))
-cls2 = eval(input("Input the number of students in class 2= ", ))
-cls3 = eval(input("Input the number of students in class 3= ", ))
+cls1,cls2,cls3 = eval(input("Input the number of students in class 1, 2, and 3 sequentially, separated by commas= ", ))
 
 a,b,c = (cls1//5), (cls2//7), (cls3//6)
 x,y,z = (cls1%5),(cls2%7),(cls3%6)
